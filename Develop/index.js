1a618b730976e72e9e042ebc905ea165598f98d4
bennettem <emily.pamelabennett@gmail.com>
2022-05-01 18:01:41 -0500

    questions mapped out

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,111 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// questions : Title of project, description -> installation instructions, usage info, contribut guideline, test instructions
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?: (REQUIRED)",
+    validate: (title) => {
+      if (title) {
+        return true;
+      } else {
+        console.log("Please enter your projects name");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "How would you describe your project?: (REQUIRED)",
+    validate: (description) => {
+      if (description) {
+        return true;
+      } else {
+        console.log("Please give your project a description");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Please give installation instructions",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Please give usage information",
+  },
+  {
+    type: "checkbox",
+    name: "license",
+    message:
+      "Please select which license that your project uses (select all that apply)",
+    choices: [
+      "Apache",
+      "Boost",
+      "BSD",
+      "Eclipse",
+      "GNU",
+      "IBM",
+      "ISC",
+      "MIT",
+      "Mozilla",
+      "SIL",
+      "Unlicense",
+    ],
+  },
+  {
+    type: "input",
+    name: "contribution",
+    message: "Enter contribution guidelines:",
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "Enter test instructions:",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "Please enter your GitHub username (required):",
+    validate: (github) => {
+      if (github) {
+        return true;
+      } else {
+        console.log("Please enter your username");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Please enter your email address (required)",
+    validate: (email) => {
+      if (email) {
+        return true;
+      } else {
+        console.log("Please enter your email");
+        return false;
+      }
+    },
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  return inquirer.prompt(questions).then((data) => {
+    console.log(data);
+  });
+}
 
 // Function call to initialize app
 init();
